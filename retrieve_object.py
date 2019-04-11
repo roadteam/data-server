066ba9cd3cff8c16c04bfa2ecceecd1dbcbd7d21
MrSpadala <pietro.spa@gmail.com>
2019-04-11 15:24:08 +0200

    style and naming

--- retrieve_object.py
+++ retrieve_object.py
@@ -21,7 +21,7 @@ def get_all_records():
 
 	paginator = client.get_paginator('list_objects_v2')
 
-	response_iterator = paginator.paginate(Bucket='road-monitoring-data')
+	response_iterator = paginator.paginate(Bucket='road-monitoring-bucket')
 
 
 	def parser_split(s):
@@ -45,7 +45,7 @@ def get_all_records():
 			pprint(obj_key)
 			print('END OBJ_KEY')
 
-			obj = s3.Object('road-monitoring-data', obj_key)
+			obj = s3.Object('road-monitoring-bucket', obj_key)
 
 			res = obj.get()
 			body = res[u'Body']
@@ -53,9 +53,7 @@ def get_all_records():
 			body_str = body.read()
 			print(type(body_str))
 			print('BEGIN OBJECT CONTENT')
-	#		print(body_str)
-	#		for c in body_str:
-	#			print(c)
+
 			l = parser_split(body_str)
 			pprint(l)
 			all_records.extend(l)
@@ -66,34 +64,4 @@ def get_all_records():
 
 	pprint(all_records)
 	return all_records
-
-'''
-s3 = boto3.resource('s3')
->>> ob = s3.Object('road-monitoring-data', u'2019/03/27/14/road-data-stream-1-2019-03-27-14-10-29-c40242f9-6de7-435e-99b3-0e3192ba1171')
-'''
-'''
-{u'Contents': [{u'ETag': '"ee9f8a8d64653932ebb75d3fd52a1de7"',
-                u'Key': u'2019/03/27/14/road-data-stream-1-2019-03-27-14-10-29-c40242f9-6de7-435e-99b3-0e3192ba1171',
-                u'LastModified': datetime.datetime(2019, 3, 27, 14, 11, 31, tzinfo=tzlocal()),
-                u'Size': 2891,
-                u'StorageClass': 'STANDARD'}],
- u'EncodingType': 'url',
- u'IsTruncated': False,
- u'KeyCount': 1,
- u'MaxKeys': 1000,
- u'Name': 'road-monitoring-data',
- u'Prefix': u'',
- 'ResponseMetadata': {'HTTPHeaders': {'content-type': 'application/xml',
-                                      'date': 'Wed, 27 Mar 2019 15:12:28 GMT',
-                                      'server': 'AmazonS3',
-                                      'transfer-encoding': 'chunked',
-                                      'x-amz-bucket-region': 'eu-west-1',
-                                      'x-amz-id-2': 'j8g8NRy1Ll0474VDoqYz20rPh6QYuraVkKOfnWXMBWriGDJE/av/xwBYQ1MeHgHnmZgnl63KrZo=',
-                                      'x-amz-request-id': '8256F5999CB0C3D1'},
-                      'HTTPStatusCode': 200,
-                      'HostId': 'j8g8NRy1Ll0474VDoqYz20rPh6QYuraVkKOfnWXMBWriGDJE/av/xwBYQ1MeHgHnmZgnl63KrZo=',
-                      'RequestId': '8256F5999CB0C3D1',
-                      'RetryAttempts': 0}}
-
-'''
 
